518b6cd5656c485764462a2fb389b384ebe1c1f7
Bipul Dhimal <102291366+lepser69420@users.noreply.github.com>
2024-03-01 20:50:18 +0545

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,49 @@
+import pytesseract
+from PIL import Image
+from langdetect import detect_langs
+
+
+
+def detect_language(word):
+    try:
+        a = detect_langs(word)
+        return a
+    except:
+        return []
+
+def extract_words_from_image(image_path):
+    text = pytesseract.image_to_string(Image.open(image_path), lang="nep+eng")
+    word = text.split()
+    return word
+
+
+print(extract_words_from_image('img.png'))
+def classify_words(words):
+    english = []
+    nepali = []
+
+    for word in words:
+        lang_list = detect_language(word)
+        #print(word, lang_list)
+        if lang_list:
+            lang = lang_list[0].lang
+            if lang == 'en':
+                english.append(word)
+            elif lang == 'ne':
+                nepali.append(word)
+
+    return english, nepali
+
+
+
+#### Image Detection
+
+import pytesseract
+from PIL import Image
+
+image_file= "R.png"
+img =Image.open(image_file)
+
+ocr_result = pytesseract.image_to_string(img)
+
+print(ocr_result)
